adb5d93cb4406646b0c3bbebbea641c3c8a26b71
Davronbek <zvezdadavid1@gmail.com>
2023-03-11 13:15:58 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// const favouriteColor = [];
+
+// favouriteColor[0] = prompt("what's your favourite color ? #1", "");
+// favouriteColor[1] = prompt("what's your favourite color ? #2", "");
+// favouriteColor[2] = prompt("what's your favourite color ? #3", "");
+
+// console.log(favouriteColor);
+
+let incr = 10;
+let decr = 10;
+
+incr++;
+decr--;
+
+
+console.log(incr);
+console.log(decr);
